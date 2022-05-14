2c6fe564ba4aed81388f95f76c071596c04d5f35
Peter Palmer <pedro.garciamaurino@students.salle.url.edu>
2022-05-14 16:58:48 +0200

    Finished trigger 4.2 and 4.3 with own validations

--- triggers/set4.sql
+++ triggers/set4.sql
@@ -43,8 +43,62 @@ END;
 $complete_mission_reward$ LANGUAGE plpgsql;
 
 -- 4.2) Batalla amb jugadors
+DROP TRIGGER IF EXISTS battle_completed ON battle;
+CREATE TRIGGER battle_completed
+    AFTER INSERT
+    ON battle
+    FOR EACH ROW
+EXECUTE FUNCTION battle_completed();
+
+CREATE OR REPLACE FUNCTION battle_completed() RETURNS TRIGGER AS
+$battle_completed$
+BEGIN
+    UPDATE player SET trophies = trophies + new.trophies_played WHERE id_player = new.winner;
+    RETURN NEW;
+END;
+$battle_completed$ LANGUAGE plpgsql;
 
 -- 4.3) Corrupció de dades
+DROP TRIGGER IF EXISTS check_inconsistencies_before_insert ON give;
+CREATE TRIGGER check_inconsistencies_before_insert
+    BEFORE INSERT
+    ON give
+    FOR EACH ROW
+EXECUTE FUNCTION check_inconsistencies();
+
+DROP TRIGGER IF EXISTS check_inconsistencies_before_update ON give;
+CREATE TRIGGER check_inconsistencies_before_update
+    BEFORE UPDATE
+    ON give
+    FOR EACH ROW
+EXECUTE FUNCTION check_inconsistencies();
+
+CREATE OR REPLACE FUNCTION check_inconsistencies() RETURNS TRIGGER AS
+$check_inconsistencies$
+BEGIN
+    IF new.gold IS NULL THEN
+        INSERT INTO warnings(affected_table, error_message, date, "user")
+        SELECT 'give',
+               'Sha realitzat una donacio dor nula.',
+               now(),
+               new.id_player;
+        RETURN NEW;
+    END IF;
+    -- test if the player who has donated belongs to the same clan
+    IF new.id_player IS NOT NULL THEN
+        IF new.id_player NOT IN (SELECT id_player FROM joins WHERE id_clan = new.id_clan) THEN
+            INSERT INTO warnings(affected_table, error_message, date, "user")
+            SELECT 'give',
+                   'Sha realitzat una donacio de ' || new.gold || ' dor a ' ||
+                   (SELECT clan_name FROM clan WHERE id_clan = new.id_clan) ||
+                   ' sha realitzat sense pertanyer al clan.',
+                   now(),
+                   new.id_player;
+        END IF;
+    END IF;
+    RETURN NEW;
+END ;
+$check_inconsistencies$ LANGUAGE plpgsql;
 
 
 --- VALIDATION OF FUNCTION 4.1 ---
@@ -94,3 +148,39 @@ WHERE id_mission = 144
 SELECT *
 FROM warnings
 WHERE affected_table = 'accepts';
+
+--- VALIDATION OF FUNCTION 4.2 ---
+
+-- 1) Let's check how many trophies has this player
+SELECT *
+FROM player
+WHERE id_player = '#9Q8UCU0Q0';
+
+-- 2) Let's create a new battle, making the player #9Q8UCU0Q0 the winner
+DELETE
+FROM battle
+WHERE id_battle = 9920;
+INSERT INTO battle (id_battle, winner, loser, datetime, duration, points, trophies_played, gold_played, clan_battle)
+VALUES (9920, '#9Q8UCU0Q0', '#PJG9RLQC', now(), '00:10:30', 100, 1150, 1500, NULL);
+
+-- 3) Let's check how many trophies has this player
+SELECT *
+FROM player
+WHERE id_player = '#9Q8UCU0Q0';
+
+-- Validation of function 4.3
+
+-- 1) We are going to insert a null value in the gold column of the give table
+DELETE FROM give WHERE id_player = '#9Q8UCU0Q0';
+INSERT INTO give (id_clan, id_player, date, gold, experience)
+VALUES ('#PV2G9U2L', '#9Q8UCU0Q0', now(), NULL, 665);
+
+-- 2) We are going to make a donation to a clan that does not belong the player with id #9Q8UCU0Q0
+SELECT * FROM joins WHERE id_player = '#9Q8UCU0Q0'; -- he belongs to a clan with id #8LGRYC
+INSERT INTO give (id_clan, id_player, date, gold, experience)
+VALUES ('#8LGRYC', '#9Q8UCU0Q0', now(), 100, 665); -- warning table should not be affected
+SELECT * FROM warnings WHERE affected_table = 'give';
+
+INSERT INTO give (id_clan, id_player, date, gold, experience)
+VALUES ('#P0LLG9RG', '#9Q8UCU0Q0', now(), 100, 665); -- warning table now should be affected
+SELECT * FROM warnings WHERE affected_table = 'give';
